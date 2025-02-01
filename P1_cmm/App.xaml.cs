9decb691508df39cf69857e5ba4b57afe27bf8d3
MERCK <21030463@itesa.edu.mx>
2025-01-31 18:45:09 -0600

    V_01: Peuwba de comit y creacion de proyecto

---
+++ P1_cmm/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace P1_cmm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ P1_cmm/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace P1_cmm
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
